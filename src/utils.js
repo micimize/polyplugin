e86afe18ef0a4b0fe96bc7de1797a8a0996989d4
micimize <rosenthalm93@gmail.com>
2016-09-28 21:55:03 +0200

    extracting code from polypacker

---
+++ src/utils.js
@@ -0,0 +1,68 @@
+export function clone(obj) {
+  if (null == obj || "object" != typeof obj) return obj;
+  var copy = obj.constructor() || {}
+  for (var attr in obj) {
+    if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+  }
+  return copy;
+}
+
+export function flatten(arrays){
+  return [].concat.apply([], arrays);
+}
+
+function objectsAreEqual(a, b) {
+  // Create arrays of property names
+  var aProps = Object.getOwnPropertyNames(a);
+  var bProps = Object.getOwnPropertyNames(b);
+
+  // If number of properties is different,
+  // objects are not equivalent
+  if (aProps.length != bProps.length) {
+    return false;
+  }
+
+  for (var i = 0; i < aProps.length; i++) {
+    var propName = aProps[i];
+
+    // If values of same property are not equal,
+    // objects are not equivalent
+    if (a[propName] !== b[propName]) {
+      return false;
+    }
+  }
+
+  // If we made it this far, objects
+  // are considered equivalent
+  return true;
+}
+
+function values(obj){
+  return Object.keys(obj).map(k => obj[k])
+}
+
+export function uniquify(list){
+  let objects = []
+  return values(list.reduce((map, item) => {
+    if(typeof(item) != 'object'){
+      return Object.assign(map, {[item]: item})
+    } else if(!objects.filter(o => objectsAreEqual(o, item)).length){
+      objects.push(item)
+      return Object.assign(map, {[item]: item})
+    } else {
+      return map
+    }
+  }, {}))
+}
+
+function mergeArrays(arrays){
+  return uniquify(flatten(arrays))
+}
+
+function mergeObjects(objects){
+  return Object.assign({}, ...objects)
+}
+
+export function merge(...args){
+  return (Array.isArray(args[0])) ? mergeArrays(args) : mergeObjects(args)
+}
