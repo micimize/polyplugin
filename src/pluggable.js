ee2748616194f10e75ce24f4e20089fc26ffc1fe
micimize <rosenthalm93@gmail.com>
2016-10-06 14:12:09 -0500

    flow?

--- src/pluggable.js
+++ src/pluggable.js
@@ -1,3 +1,4 @@
+// @flow
 import readJsonSync from 'read-json-sync'
 import { merge, identity } from './utils'
 import * as resolve from './resolve'
@@ -9,19 +10,19 @@ export default function pluggable({
   resolver = _ => _,
   file = process.env.CONFIGURATION_FILE || './package.json',
   sources = [{path: process.env.CONFIGURATION_PATH || '$.polypacker'}]
-}){
+}: any){
   let json = readJsonSync(file)
   return merger(defaults, handler(resolve.sources(json, sources)))
 }
 
-export function fromSingleSource({path, resolver, ...rest}){
+export function fromSingleSource({path, resolver, ...rest}: any){
   return pluggable({
     sources: [ { path, resolver } ],
     ...rest,
   })
 }
 
-export const list = obj => fromSingleSource({ ...obj, resolver: resolve.toList })
+export const list = (obj: any) => fromSingleSource({ ...obj, resolver: resolve.toList })
 
-export const map = obj => fromSingleSource({ ...obj, resolver: resolve.toMap })
+export const map = (obj: any) => fromSingleSource({ ...obj, resolver: resolve.toMap })
 
